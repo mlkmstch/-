4393b4bfc7ffceb488025624c500e4c5c6b81f8b
Pepis <mlkmstch106@gmail.com>
2023-04-28 18:56:01 +0700

    Дополнение форм

--- ToDolist1/Form1.cs
+++ ToDolist1/Form1.cs
@@ -12,9 +12,38 @@ namespace ToDolist1
 {
     public partial class Form1 : Form
     {
+        Create create = Create.instance;
+        MainScreen mainScreen = MainScreen.instance;
+        Note note = Note.instance;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Controls.Add(create);
+            create.Hide();
+            this.Controls.Add(mainScreen);
+            mainScreen.Hide();
+            this.Controls.Add(note);
+            note.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mainScreen.Show();
+            mainScreen.BringToFront();
+            //this.Hide();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            create.Show();
+            create.BringToFront();
+            //this.Hide();
+            
         }
     }
 }
