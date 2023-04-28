321451495a43139ceb5a5a77314aa9e2bbd24ae0
Pepis <mlkmstch106@gmail.com>
2023-04-28 18:56:01 +0700

    Sanya hui sosi

---
+++ ToDolist1/Create.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDolist1
+{
+    public partial class Create : UserControl
+    {
+        private static Create _instance;
+        public static Create instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Create();
+                return _instance;
+            }
+        }
+        public Create()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ToDolist1/MainScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDolist1
+{
+    public partial class MainScreen : UserControl
+    {
+        private static MainScreen _instance;
+        public static MainScreen instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new MainScreen();
+                return _instance;
+            }
+        }
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
